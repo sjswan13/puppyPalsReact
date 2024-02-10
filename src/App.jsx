cc6dfc14d395f49c1da7dc7b4efa76f1d27e4058
sjswan13 <sjswan13@gmail.com>
2024-02-09 16:39:48 -0800

    react and styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
+import './index.css'
 
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList)
-  
+  const [featPupID, setFeatPupID] = useState(null);
+
+
   console.log(puppyList, 'puppies');
+
+  const handleClick = (puppyID) => {
+    console.log('puppy id:', puppyID);
+    setFeatPupID(puppyID);
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupID)
+  console.log(featuredPup)
+
   return (
-      <div className='App'>
+    <div className='App'>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-          })
-        }
+            return (
+            <p onClick={() => handleClick(puppy.id)}
+             key={puppy.id}>
+              {puppy.name}
+            </p>
+            );
+          })}
+         {featPupID && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+         )}
       </div>
   )
 }
